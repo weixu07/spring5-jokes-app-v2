fe6f61de8a45d05b87b60ad6070609a4d0381dc9
weixu07 <weixu07@gmail.com>
2021-04-04 00:18:42 -0400

    set up project. cloese #1

---
+++ src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring5jokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class Spring5JokesAppV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesAppV2Application.class, args);
+    }
+
+
+}
